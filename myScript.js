c2538777875c8789c7327ca8578af82c2b681fb6
Camille James <james2@southwestern.edu>
2025-06-27 19:53:29 -0400

    start

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function toProjects(){
+    window.location.href = "file:///C:/Users/cjam2/index.html/projects.html";
+}
+function toHome(){
+    window.location.href = "file:///C:/Users/cjam2/index.html/index.html";
+}
+function toSummer(){
+    window.location.href = "file:///C:/Users/cjam2/index.html/summer.html";
+}
+function toHobbies(){
+    window.location.href = "file:///C:/Users/cjam2/index.html/hobbies.html";
+}
